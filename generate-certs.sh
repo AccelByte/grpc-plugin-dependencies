1bdfb8d34376ba5a2c0de8a407762b223e9dc5ac
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2022-12-08 17:15:39 +0700

    fix(generate-certs): fix permission for envoy container

--- generate-certs.sh
+++ generate-certs.sh
@@ -30,11 +30,11 @@ mkdir -p "$CERT_DIR"
 
 # openssl genrsa -out "$CERT_DIR/client.key" 2048
 
-echo -e -n "Create CA key..."
+echo -e -n "Create CA key ... "
 openssl genrsa -out "$CERT_DIR/$CERT_CA_KEY_FILE" 4096 2> /dev/null
 echo -e "OK"
 
-echo -e -n "Create CA certificate..."
+echo -e -n "Create CA certificate ... "
 CA_SUBJECT="/C=$CERT_COUNTRY/O=$CERT_ORG/CN=$CERT_ROOT_CN"
 openssl req -x509 -new -nodes -sha256 -days "$CERT_CA_DAYS" \
     -key "$CERT_DIR/$CERT_CA_KEY_FILE" \
@@ -42,11 +42,11 @@ openssl req -x509 -new -nodes -sha256 -days "$CERT_CA_DAYS" \
     -subj "$CA_SUBJECT"
 echo -e "OK"
 
-echo -e -n "Create server key..."
+echo -e -n "Create server key ... "
 openssl genrsa -out "$CERT_DIR/$CERT_SV_KEY_FILE" 2048 2> /dev/null
 echo -e "OK"
 
-echo -e -n "Create server certificate..."
+echo -e -n "Create server certificate ... "
 SERVER_SUBJECT="/C=$CERT_COUNTRY/O=$CERT_ORG"
 openssl req -new \
     -key "$CERT_DIR/$CERT_SV_KEY_FILE" \
@@ -61,14 +61,14 @@ openssl x509 -req -CAcreateserial -sha256 -days "$CERT_DAYS" \
     2> /dev/null
 echo -e "OK"
 
-echo -e -n "Validate server certificate... "
+echo -e -n "Validate server certificate ... "
 openssl verify -verbose -CAfile "$CERT_DIR/$CERT_CA_PEM_FILE" "$CERT_DIR/$CERT_SV_PEM_FILE"
 
-echo -e -n "Create client key..."
+echo -e -n "Create client key ... "
 openssl genrsa -out "$CERT_DIR/$CERT_CL_KEY_FILE" 2048 2> /dev/null
 echo -e "OK"
 
-echo -e -n "Create client certificate..."
+echo -e -n "Create client certificate ... "
 CLIENT_SUBJECT="/C=$CERT_COUNTRY/O=$CERT_ORG"
 openssl req -new \
     -key "$CERT_DIR/$CERT_CL_KEY_FILE" \
@@ -83,10 +83,12 @@ openssl x509 -req -CAcreateserial -sha256 -days "$CERT_DAYS" \
     2> /dev/null
 echo -e "OK"
 
-echo -e -n "Validate client certificate... "
+echo -e -n "Validate client certificate ... "
 openssl verify -verbose -CAfile "$CERT_DIR/$CERT_CA_PEM_FILE" "$CERT_DIR/$CERT_CL_PEM_FILE"
 
 rm -rf "$CERT_DIR/$CERT_SV_CSR_FILE"
 rm -rf "$CERT_DIR/$CERT_CL_CSR_FILE"
 
-echo -e "Finished"
+echo -e "Fixing permissions for Envoy proxy container ... "
+chmod -v 644 $CERT_DIR/*
+echo -e "OK"
